6ed1c30d5ea3886d0eb327ebe45b660020f38361
mayeku-vicent <mayekuvicent4@gmail.com>
2026-09-18 00:58:48 -0900

    Fix ALLOWED_HOSTS to read from environment variable

--- source/config/settings.py
+++ source/config/settings.py
@@ -17,8 +17,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY', default='django-insecure-change-this-in-production-key-xyz123')
 
 DEBUG = env('DEBUG', default=True)
-
-ALLOWED_HOSTS = [host.strip() for host in os.environ.get("ALLOWED_HOSTS", "*").split(",") if host.strip()]
+import os
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get("ALLOWED_HOSTS", "*").split(",") if h.strip()]
 
 SECRET_KEY = 'django-insecure-ea8cs3!!7uy52k8!2%3x$#-$myi^&9^u*vl(*e=(63vy#=x9zz'
 
