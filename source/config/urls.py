87b21979522acbf4880308ec171b559cdd82cba1
mayeku-vicent <mayekuvicent4@gmail.com>
2026-09-18 01:19:10 -0900

    Add root URL redirect to resolve 404 error

--- source/config/urls.py
+++ source/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,7 +9,8 @@ from config.views import api_root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', api_root, name='api-root'), # <--- Ensure the trailing slash is here
+    path('', lambda request: redirect('admin/')),
+    path('api/', api_root, name='api-root'), 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/organization/', include('organization.urls')),
